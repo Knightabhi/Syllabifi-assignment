02cb86a2f5fa713435faf4f0dbdf7bec4398a92d
Knight <geetaabhinav2001@gmail.com>
2021-07-23 17:17:27 +0530

    commit

--- src/components/Dashboard/Practices.jsx
+++ src/components/Dashboard/Practices.jsx
@@ -24,7 +24,49 @@ const useStyles = makeStyles((theme) => ({
 function Practices() {
   const classes = useStyles();
   return (
-    <Container className={` ${classes.card} bg-white  pl-2 shadow`}></Container>
+    <Container className={` ${classes.card} bg-white  pl-2 shadow`}>
+      <Row className="mb-3 mt-2 text-start">
+        <Typography variant="h4">Best Practices </Typography>
+      </Row>
+      <br />
+      <Row className="mb-3 mt-2  border-bottom p-2">
+        <Col className="text-start">
+          {" "}
+          <Typography variant="h6">Tips for Solving questions </Typography>
+        </Col>
+        <Col className="text-end">
+          {" "}
+          <Typography variant="h4">+</Typography>
+        </Col>
+      </Row>
+      <Row className="mb-3 mt-2  border-bottom p-2">
+        <Col className="text-start">
+          <Typography variant="h6">How many seats are available? </Typography>
+        </Col>
+        <Col className="text-end">
+          {" "}
+          <Typography variant="h4">+</Typography>
+        </Col>
+      </Row>
+      <Row className="mb-3 mt-2 text-start border-bottom p-2">
+        <Col className="text-start">
+          <Typography variant="h6">Strategies for error finding </Typography>
+        </Col>
+        <Col className="text-end">
+          {" "}
+          <Typography variant="h4">+</Typography>
+        </Col>
+      </Row>
+      <Row className="mb-3 mt-2 text-start border-bottom p-2">
+        <Col className="text-start">
+          <Typography variant="h6">How to make Video solutions</Typography>
+        </Col>
+        <Col className="text-end">
+          {" "}
+          <Typography variant="h4">+</Typography>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
